7bb0f6e70a01dc680eef27e29505215c607a557e
royalrobotics2522 <RoyalRobotics@outlook.com>
2026-02-05 19:33:15 -0800

    Added Motors to Spinderxer & Turret

--- src/main/java/frc/robot/subsystems/Spindexer.java
+++ src/main/java/frc/robot/subsystems/Spindexer.java
@@ -1,10 +1,17 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Spindexer extends SubsystemBase {
+    private TalonFX SpinnerMotor;
+    private StatusSignal SpinSignal;
+
     public Spindexer() {
-        
+        SpinnerMotor = new TalonFX(0);
+        SpinSignal = SpinnerMotor.getPosition();
     }
     public void periodic() {
         
--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -1,13 +1,33 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Turret extends SubsystemBase{
-    public Turret() {
+    private TalonFX TurretAngleMotor;
+    private TalonFX TurretHoodMotor;
+    private TalonFX TurretShooterMotor;
+    private TalonFX TurretShooterFollowerMotor;
 
+    private CANcoder TurrentAngleSmall;
+    private CANcoder TurrentAngleBig;
+    private StatusSignal TurretAngleSignal;
+
+    public Turret() {
+        TurretAngleMotor = new TalonFX(0);
+        TurretHoodMotor = new TalonFX(0);
+        TurretShooterMotor = new TalonFX(0);
+        TurretShooterFollowerMotor = new TalonFX(0);
+
+        TurrentAngleSmall = new CANcoder(0);
+        TurrentAngleBig = new CANcoder(0);
+        TurretAngleSignal = TurretAngleMotor.getPosition();
     }
     public void periodic() {
 
     }
-    
+
 }
