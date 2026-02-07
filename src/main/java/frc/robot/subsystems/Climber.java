f41d01ed9614f55200ac4a684c1a73fe361aed39
royalrobotics2522 <RoyalRobotics@outlook.com>
2026-02-06 19:44:34 -0800

    Added configs

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -7,10 +9,16 @@ import pabeles.concurrency.ConcurrencyOps.NewInstance;
 
 public class Climber extends SubsystemBase{
    
-    private TalonFX ClimberMotor; // wench motor
+    private TalonFX ClimberMotor; 
+    
+    //motor configs
+    private MotorOutputConfigs IntakeMotorConfig= new MotorOutputConfigs();
+    private CurrentLimitsConfigs IntakeCurrentConfig= new CurrentLimitsConfigs();
 
     public Climber() {
-        ClimberMotor = new TalonFX(0);
+      ClimberMotor = new TalonFX(0);
+        ClimberMotor.getConfigurator().apply (IntakeMotorConfig);
+        ClimberMotor.getConfigurator().apply(IntakeCurrentConfig);
   }
 
   public void periodic() {
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,28 +1,42 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MagnetSensorConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase{
-   
+    //motors
     private TalonFX IntakeMotorLift;
-    private TalonFX IntakeMotorSpin; //motors
+    private TalonFX IntakeMotorSpin;
 
+    //motor configs
+    private MotorOutputConfigs IntakeMotorConfig= new MotorOutputConfigs();
+    private CurrentLimitsConfigs IntakeCurrentConfig= new CurrentLimitsConfigs();
+    private Slot0Configs IntakePidConfigs= new Slot0Configs();
+  
+    //CAN configs
+    private MagnetSensorConfigs IntakeCANMagnetSensor= new MagnetSensorConfigs();
     private CANcoder IntakeLiftEncoder;
-    
-    private StatusSignal IntakeLiftData;
 
+    //Intake classifier
     public Intake() {
         IntakeMotorLift = new TalonFX(0);
+            IntakeMotorSpin.getConfigurator().apply(IntakeMotorConfig);
+            IntakeMotorSpin.getConfigurator().apply(IntakeCurrentConfig);
+        
         IntakeMotorSpin = new TalonFX(0);
+            IntakeMotorLift.getConfigurator().apply(IntakeMotorConfig);
+            IntakeMotorLift.getConfigurator().apply(IntakeCurrentConfig);
+            IntakeMotorLift.getConfigurator().apply(IntakePidConfigs);
 
         IntakeLiftEncoder = new CANcoder(0);
-
-        IntakeLiftData = IntakeMotorLift.getPosition ();
-    
+            IntakeLiftEncoder.getConfigurator().apply(IntakeCANMagnetSensor);
     }
 
     public void periodic () {
